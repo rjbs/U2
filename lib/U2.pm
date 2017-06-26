126393a44daf7e974b27307ae0fa5d9ecbd4aca4
Ricardo Signes <rjbs@cpan.org>
2017-06-26 10:37:25 -0400

    long-lost work on u2 from_str

--- lib/U2.pm
+++ lib/U2.pm
@@ -13,6 +13,38 @@ sub from_struct {
   bless $s => $class;
 }
 
+sub from_string {
+  my ($class, $str) = @_;
+  
+  $str =~ m{\A(\w+):([^?]*)(?:\?([^#]*))?(?:#(.+))?\z};
+
+  my %arg = (
+    scheme => $1,
+    hier   => $2,
+    query  => $3,
+    frag   => $4,
+  );
+
+  if ($arg{hier} =~ m{\A//(?:([^@]+)@)?([^:]*?)(?::(\d+))?(/.+)?\z}) {
+    $arg{authority} = {
+      userinfo => $1,
+      host     => $2,
+      port     => $3,
+    };
+
+    $arg{path} = [ split m{/}, $4 ];
+
+    if (@{ $arg{path} } and $arg{path}[0] eq '') {
+      shift @{ $arg{path} };
+    } else {
+      $arg{relative} = 1;
+    }
+  }
+
+  use YAML::XS;
+  warn Dump(\%arg);
+}
+
 sub as_string {
   my ($self) = @_;
 
